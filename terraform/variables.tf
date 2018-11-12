6fd627b1d2f474c470328f14a9f966f49ba567e0
glennbech <xp8fda>
2018-11-12 05:16:37 +0100

    new

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 # Modify this file to make it your own :-)
 
 variable "app_prefix" {
-  default = "pgr301-xxx"
+  default = "pgr301-zzz"
 }
 
 variable "pipeline_name" {
-  default = "pgr301-your-initials"
+  default = "pgr301-your-initialsz"
 }
